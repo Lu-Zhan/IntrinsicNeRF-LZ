637191c0b9305aa7febc5a658847d3cc629384d1
Lu Zhan <zhannixl@gmail.com>
2024-06-18 01:08:51 +0800

    update

--- object_level/scripts/c0.sh
+++ object_level/scripts/c0.sh
@@ -6,6 +6,7 @@ data_type="real_data"
 
 dataset_name=new_dog
 mkdir logs/${data_type}_${dataset_name}
+CUDA_VISIBLE_DEVICES=${device} python run_nerf.py --config configs/${dataset_name}.txt --exp ${data_type}_${dataset_name} --datadir ${dataset_dir}/${data_type}/${dataset_name}
 CUDA_VISIBLE_DEVICES=${device} python run_nerf.py --config configs/${dataset_name}.txt --exp ${data_type}_${dataset_name} --datadir ${dataset_dir}/${data_type}/${dataset_name} --render_only --render_test --testskip 1
 
 # dataset_name=lego
